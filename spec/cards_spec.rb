2138e8e7804740fdffd890198005eebe06079b59
jerry <jerry@usertesting.com>
2014-06-05 13:53:27 -0700

    added shared examples for card deck basics

--- spec/cards_spec.rb
+++ spec/cards_spec.rb
@@ -1,38 +1,72 @@
 require 'spec_helper'
 
 describe Cards do
-  let(:cards) { Cards.new(33) }
-
-  it "can be instantiated" do
-    expect(cards).to be_a Cards
-  end
-
-  it "returns an array of cards" do
-    expect(cards.get_cards).to be_a(Array)
-    expect(cards.get_cards[0]).to be_a(Card)
-  end
-
-  it "has the right number of card elements" do
-    expect(cards.get_cards.count).to eq(33)
-  end
-
-  context "standard card types" do
-    it "creates a standard 52-card playing card deck" do
-      cards = Cards.new(:playing_cards)
-      expect(cards.get_cards.count).to eq(52)
+  shared_examples_for "a usable deck" do
+    it "creates an array" do
+      expect(deck.get_cards).to be_a(Array)
     end
 
-    it "creates a standard 54-card playing card deck with jokers" do
-      cards = Cards.new(:playing_cards_with_jokers)
-      expect(cards.get_cards.count).to eq(54)
+    it "creates an array of Card objects" do
+      deck.get_cards.each do |card|
+        expect(card).to be_a(Card)
+      end
     end
 
-    it "creates a standard 108-card Uno card deck" do
-      cards = Cards.new(:uno)
-      expect(cards.get_cards.count).to eq(108)
+    it "creates an array of different Card objects" do
+      expect(deck.get_cards.uniq.count).to eq(deck.get_cards.count)
+    end
+  end
+
+  context "standard deck types" do
+    context "standard deck" do
+      let(:cards) { Cards.new(:playing_cards) }
+
+      it_behaves_like "a usable deck" do
+        let(:deck) { cards }
+      end
+
+      it "creates a standard 52-card playing card deck" do
+        expect(cards.get_cards.count).to eq(52)
+      end
     end
 
+    context "standard deck with jokers" do
+      let(:cards) { Cards.new(:playing_cards_with_jokers) }
 
+      it_behaves_like "a usable deck" do
+        let(:deck) { cards }
+      end
+
+      it "creates a standard 54-card playing card deck with jokers" do
+        expect(cards.get_cards.count).to eq(54)
+      end
+    end
+
+    context "standard uno deck" do
+      let(:cards) { Cards.new(:uno) }
+
+      it_behaves_like "a usable deck" do
+        let(:deck) { cards }
+      end
+
+      it "creates a standard 108-card Uno card deck" do
+        expect(cards.get_cards.count).to eq(108)
+      end
+    end
+  end
+
+  context "custom desk types" do
+    context "standard deck" do
+      let(:cards) { Cards.new(33) }
+
+      it_behaves_like "a usable deck" do
+        let(:deck) { cards }
+      end
+
+      it "has the right number of card elements" do
+        expect(cards.get_cards.count).to eq(33)
+      end
+    end
   end
 
 end
