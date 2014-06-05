d6d8270ff623a1436cdfbf23e11bdae74de03188
jerry <jerry@usertesting.com>
2014-06-05 13:19:26 -0700

    Add standard deck types

--- app/cards.rb
+++ app/cards.rb
@@ -1,3 +1,35 @@
 class Cards
 
+  DECK_TYPES = [
+    :playing_cards,
+    :playing_cards_with_jokers,
+    :uno,
+    :custom
+  ]
+
+  DECK_SIZES = {
+    playing_cards: 52,
+    playing_cards_with_jokers: 54,
+    uno: 108,
+    custom: 0
+  }
+
+  def initialize(deck_type_or_number_of_cards)
+    if deck_type_or_number_of_cards.is_a?(Symbol)
+      @deck_size = DECK_SIZES[deck_type_or_number_of_cards]
+      @deck_type = deck_type_or_number_of_cards
+    else
+      @deck_size = deck_type_or_number_of_cards
+      @deck_type = :custom
+    end
+  end
+
+  def get_cards
+    array = []
+    (1..@deck_size).each do
+      array << Card.new
+    end
+    array
+  end
+
 end
--- spec/cards_spec.rb
+++ spec/cards_spec.rb
@@ -1,9 +1,38 @@
 require 'spec_helper'
 
 describe Cards do
+  let(:cards) { Cards.new(33) }
+
   it "can be instantiated" do
-    c = Cards.new
-    expect(c).to be_a Cards
+    expect(cards).to be_a Cards
+  end
+
+  it "returns an array of cards" do
+    expect(cards.get_cards).to be_a(Array)
+    expect(cards.get_cards[0]).to be_a(Card)
+  end
+
+  it "has the right number of card elements" do
+    expect(cards.get_cards.count).to eq(33)
+  end
+
+  context "standard card types" do
+    it "creates a standard 52-card playing card deck" do
+      cards = Cards.new(:playing_cards)
+      expect(cards.get_cards.count).to eq(52)
+    end
+
+    it "creates a standard 54-card playing card deck with jokers" do
+      cards = Cards.new(:playing_cards_with_jokers)
+      expect(cards.get_cards.count).to eq(54)
+    end
+
+    it "creates a standard 108-card Uno card deck" do
+      cards = Cards.new(:uno)
+      expect(cards.get_cards.count).to eq(108)
+    end
+
+
   end
 
 end
